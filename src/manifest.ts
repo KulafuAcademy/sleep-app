ea2ca9857037c56321e24c226a0e8d2bc27f915c
KulafuAcademy <matsuo.tomoyuki@gmail.com>
2026-05-15 15:53:09 +0800

    feat: add web app manifest

---
+++ src/manifest.ts
@@ -0,0 +1,21 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "hibiki.rest",
+    short_name: "HIBIKI",
+    description: "ambient space for rest & focus",
+    start_url: "/",
+    scope: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/apple-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  };
+}
