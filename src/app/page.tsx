3432783385f01ee8ed6a42aa0c9d983f432421c8
KulafuAcademy <matsuo.tomoyuki@gmail.com>
2026-05-16 22:56:07 +0800

    test: remove forest a2 a3 layers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -156,9 +156,9 @@ export default function Home() {
 
     const audios: HTMLAudioElement[] = [a1, b1, c1];
 
-    if (folder === "forest") {
-      a2 = new Audio(`/sound/${folder}/v1/a2.wav`);
-      a3 = new Audio(`/sound/${folder}/v1/a3.wav`);
+    //if (folder === "forest") {
+    //  a2 = new Audio(`/sound/${folder}/v1/a2.wav`);
+    //  a3 = new Audio(`/sound/${folder}/v1/a3.wav`);
 
       //a2.addEventListener("loadedmetadata", (event) => {
       //  const audio = event.currentTarget as HTMLAudioElement;
@@ -170,8 +170,9 @@ export default function Home() {
        // audio.currentTime = 149;
       //});
 
-      audios.push(a2, a3);
-    } else if (folder !== "bonfire" && folder !== "cave") {
+     // audios.push(a2, a3);
+ 
+      if (folder !== "bonfire" && folder !== "cave" && folder !== "forest") {
       a2 = new Audio(`/sound/${folder}/v1/a2.wav`);
       a3 = new Audio(`/sound/${folder}/v1/a3.wav`);
 
